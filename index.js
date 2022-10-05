18e516bdcc11afecf8b4ff43a5ac54b7dd5cb046
Usman Malik <usmanmaliksahib2611@gmail.com>
2022-10-05 12:46:34 -0700

    Api Hosting

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+const port = process.env.PORT || 3000;
+
+const apiData = require('./data.json');
+
+app.get('/', (req, res) => {
+  res.send(
+    'Welcome to my API. Type "/service" at the end of url for API data.'
+  );
+});
+
+app.get('./service', (req, res) => {
+  res.send(apiData);
+});
+
+app.listen(port, () => {
+  console.log('I am live again');
+});
